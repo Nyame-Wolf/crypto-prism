c0bba327a6c1d80103eb5dc211b4fd54ebca9b2d
Nyame-Wolf <mumenyam@gmail.com>
2022-08-29 10:54:51 +0300

    Create a store and import provider in index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+    <Provide store={store}>
+       <App />
+    </Provide>
     </BrowserRouter>
   </React.StrictMode>,
 );
