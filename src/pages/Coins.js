935ab7d6aec06c5a981cd713a8a98293db793662
Nyame-Wolf <mumenyam@gmail.com>
2022-09-01 12:45:40 +0300

    Dispatch coins from the store
    Use useLocation hook to send data to detail page
    
    create a filter search functionality to retrieve coins based on their names

---
+++ src/pages/Coins.js
@@ -0,0 +1,52 @@
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import Coin from '../components/coin/Coin';
+import { getCoins } from '../redux/coins/coinsReducer';
+import './Coins.css';
+
+const Coins = () => {
+  const dispatch = useDispatch();
+
+  const { lifecycle, coins, info } = useSelector((state) => state.coins);
+  const [search, setSearch] = useState('');
+  const filteredCoins = coins.filter(
+    (coin) => !search || new RegExp(search, 'ig').test(coin.name),
+  );
+  useEffect(() => {
+    if (!coins.length && lifecycle.loading === 'initial') {
+      dispatch(getCoins());
+    }
+  }, []);
+
+  return (
+    <div>
+      <div className="container">{`Total Coins: ${info.coins_num}`}</div>
+      <input
+        type="search"
+        value={search}
+        onChange={(e) => {
+          setSearch(e.target.value);
+        }}
+        className="search"
+      />
+
+      <ul className="coin-container">
+        {filteredCoins.map((coin) => (
+          <Link
+            to="coindetail"
+            key={coin.id}
+            state={{ coin, info }}
+            className="individual-coin"
+          >
+            <li>
+              <Coin coin={coin} />
+            </li>
+          </Link>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Coins;
